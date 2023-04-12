e48ef9a4115265c14b7e1206dd08a70a6066bb95
dohaugustin <122422951+dohaugustin@users.noreply.github.com>
2023-04-11 22:46:20 -0400

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "newinstance" {
   ami = "ami-016eb5d644c333ccb"
   instance_type = "t2.micro"
-  security_groups = ["launch-wizard-2"]
+  security_groups = ["launch-wizard-13","launch-wizard-12"]
   key_name = "Ansible-New"
   tags = {
     Name = "tomcatserver"
