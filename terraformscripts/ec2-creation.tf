e5db84e828351ea83011fb576be8cef11ca1ba1c
dohaugustin <122422951+dohaugustin@users.noreply.github.com>
2023-11-04 22:54:03 -0400

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 }
 resource  "aws_instance" "ec2instance"{
 ami = "ami-05c13eab67c5d8861"
-key_name="window-key"
+key_name="EC2-KEY"
 security_groups = ["launch-wizard"]
 instance_type = "t2.micro"
 tags = {
