189f218a68ed5984e0cfcd3af809cb3b1af53dc0
dohaugustin <122422951+dohaugustin@users.noreply.github.com>
2023-11-04 23:02:42 -0400

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 resource  "aws_instance" "ec2instance"{
 ami = "ami-05c13eab67c5d8861"
 key_name="EC2-KEY"
-security_groups = ["launch-wizard"]
+security_groups = ["launch-wizard-2"]
 instance_type = "t2.micro"
 tags = {
 Name = "tomcatserver"
